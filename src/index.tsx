7fb4b269e2f1dd2a442a6705d84ab02d5a6344c9
Wilbert Navarrete <wilmion92@gmail.com>
2021-03-26 05:35:54 +0100

    terminando configuracion

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app:HTMLElement = document.getElementById('app');
+
+ReactDOM.render( <p>Hello world</p> , app);
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const CssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname , 'dist'),
+        filename: "[fullhash].js",
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.tsx' , 'ts' , '.js' , '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    CssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'public/index.html'
+        }),
+        new CssExtractPlugin({
+            filename: 'css/[fullhash].css',
+            linkType: 'text/css'
+        })
+    ],
+    devServer: {
+        port: 8080,
+        open:true,
+        historyApiFallback: true
+    }
+}
