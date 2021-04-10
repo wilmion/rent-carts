4b34fa0ab804179adcc497a15d5c4fffcbd25bfc
Wilbert Navarrete <wilmion92@gmail.com>
2021-04-10 03:27:20 +0200

    avanzando payments

--- src/pages/AdminPayments.tsx
+++ src/pages/AdminPayments.tsx
@@ -13,7 +13,19 @@ interface IProps {
 const AdminPayments:React.FC<IProps> = (props) => {
     return (
         <section className="admin-payments">
-            Payments work's
+            <h2 className="admin-payments__title">Payment's</h2>
+            <section className="admin-payments-contain">
+                <article className="admin-payments-contain-item">
+                    <div className="admin-payments-contain-item__image">
+                        <img src="https://www.paypalobjects.com/webstatic/icon/pp258.png" alt="icon"/>
+                    </div>
+                    <section className="admin-payments-contain-item-information">
+                        <h4 className="admin-payments-contain-item-information__title">PAYPAL</h4>
+                        <h5 className="admin-payments-contain-item-information__data">yWLdLwgitBnKNs3we0wGE4iAkCW1X1yW</h5>
+                        <button className="admin-payments-contain-item-information__edit">EDIT</button>
+                    </section>
+                </article>
+            </section>
         </section>
     )
 }
