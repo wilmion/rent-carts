5e9d5e6fff31246ac7ffdf9d3383ffc67acf770d
Wilbert Navarrete <wilmion92@gmail.com>
2021-04-12 04:22:58 +0200

    finish styles

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,23 +1,65 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { useHistory } from 'react-router-dom';
 
-import { ICart, IState } from '../models/interface';
-import "../sass/pages/cart-shop.scss";
+import { PayPalButton , PaypalOptions } from 'react-paypal-button-v2'
+import Loading from '../components/Loading'
+import ErrorWindow from '../components/ErrorWindow';
+import WarningWindow from '../components/WarningWindows';
+
+import CartCardMoreDetails from '../components/CartCardMoreDetails';
+
+import { decrypt } from '../utils/decrypt';
+
+import { ICart, IPayment, IPaymentApi, IState } from '../models/interface';
+import "../sass/pages/payment.scss";
 
 interface IProps {
-    product:ICart | null
+    product:ICart | null,
+    payments:IPaymentApi[]
 }
 
 const Payment:React.FC<IProps> = (props) => {
+
+    const history = useHistory()
+
+    if(props.payments.length === 0 || !props.product) return <Loading />;
+
+    const generatePropsPaypalOptions = ():PaypalOptions => {
+
+        const payment:IPayment = decrypt(props.payments[0] , process.env.CRYPTO_SECRET);
+
+        const config:PaypalOptions = {
+            clientId: payment.data.clientID,
+            intent: 'capture',
+            currency: "USD"
+        }
+
+        return config;
+    }
+
     return (
         <section className="product-payment">
-            product payment shop work's
+            <h2 className="product-payment__title">Checkout</h2>
+            <section className="product-payment-product">
+                <CartCardMoreDetails {...props.product} />
+                <p className="product-payment-product__direction">Direction : {props.product.address.toUpperCase()}</p>
+            </section>
+            <h4 className="product-payment__subtitle">Payment with Paypal</h4>
+            <section className="product-payment-method">
+                <PayPalButton
+                 options={generatePropsPaypalOptions()} 
+                 style={{layout:"vertical" , shape:'rect'}} 
+                 amount={`${props.product.price}`}
+                />
+            </section>
         </section>
     )
 }
 
 const mapStateToProps = (state:IState) => ({
-    product: state.product
+    product: state.product,
+    payments: state.payments
 })
 const mapDispatchToProps = {
 
